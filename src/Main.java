98c5bb0c33ac389dd237b7efae4963f4b681cdc1
Practice test <bonniebraer@gmail.com>
2023-11-07 15:33:19 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int milespromoney = 20; // за каждые 20 рублей 1 миля
+        int ticketprise = 12000;
+        int bonus = ticketprise/milespromoney;
+
+        System.out.println((bonus) + "миль");
+
+
+
+     }
+}
