1bb3bede5664be9c691b5f8e8e5954007b697c54
Practice test <bonniebraer@gmail.com>
2023-11-08 15:01:14 +0300

    Change Main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int milespromoney = 20; // за каждые 20 рублей 1 миля
-        int ticketprise = 12000;
-        int bonus = ticketprise/milespromoney;
+        int milesProMoney = 20; // за каждые 20 рублей 1 миля
+        int ticketPrise = 12000;
+        int bonus = ticketPrise / milesProMoney;
 
         System.out.println((bonus) + "миль");
 
 
-
-     }
+    }
 }
